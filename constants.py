e2498354cc4813ba139933bd8ed65979b585be74
LT-Pandas <john.payton747@gmail.com>
2025-07-24 11:59:04 -0400

    Rename dividend filter labels to dividend yield

--- constants.py
+++ constants.py
@@ -11,8 +11,8 @@ LABEL_TO_KEY = {
     "Upper Volume": "volumeLowerThan",
     "Lower Beta": "betaMoreThan",
     "Upper Beta": "betaLowerThan",
-    "Lower Dividend": "dividendMoreThan",
-    "Upper Dividend": "dividendLowerThan",
+    "Lower Dividend Yield": "dividendMoreThan",
+    "Upper Dividend Yield": "dividendLowerThan",
     # Dropdowns + Boolean filters
     "Sector": "sector",
     "Industry": "industry",
